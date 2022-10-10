937d579ed0ae8a2c40e8ed2ed1e9ac3d98f0dfbb
byarm <matis.byar@etu.umontpellier.fr>
2022-10-10 18:13:27 +0200

    Ajout du diagramme de classes Renommage de la classe Brique en Piece

---
+++ Piece.java
@@ -0,0 +1,7 @@
+/**
+ * La classe **enum** Piece contient toutes les formes de pièces. Sa nature enum est un choix qui nous permet de créer
+ * plusieurs catégories de pièces.
+ */
+public enum Piece {
+
+}
