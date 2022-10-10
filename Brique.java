bf4eb78f6eabe287bac451eb1f3da6e9e6916cad
byarm <matis.byar@etu.umontpellier.fr>
2022-10-10 17:37:09 +0200

    Création et description brève des classes

---
+++ Brique.java
@@ -0,0 +1,7 @@
+/**
+ * La classe **enum** contient toutes les formes de briques. Sa nature enum est un choix qui nous permet de créer
+ * plusieurs catégories de briques.
+ */
+public enum Brique {
+
+}
---
+++ Jeu.java
@@ -0,0 +1,6 @@
+/**
+ * La classe Jeu instancie tous les éléments du jeu (Grille, Briques, ...) afin d'assurer le bon fonctionnement du jeu.
+ */
+public class Jeu {
+
+}
