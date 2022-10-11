6a85e8770a210e3d899d4f20ede50bd8477912bc
ValentinG <valentin.germain01@etu.umontpellier.fr>
2022-10-11 18:53:23 +0200

    création de la classe Plateau et ajout des pièces dans la classe Piece

--- Piece.java
+++ Piece.java
@@ -3,5 +3,44 @@
  * plusieurs catégories de pièces.
  */
 public enum Piece {
+    I(new boolean[][]{{true, true, true, true}}, "I"),
+    O(new boolean[][]{{true, true}, {true, true}}, "O"),
+    T(new boolean[][]{{false, true, false}, {true, true, true}}, "T"),
+    S(new boolean[][]{{false, true, true}, {true, true, false}}, "S"),
+    Z(new boolean[][]{{true, true, false}, {false, true, true}}, "Z"),
+    J(new boolean[][]{{true, false, false}, {true, true, true}}, "J"),
+    L(new boolean[][]{{false, false, true}, {true, true, true}}, "L");
 
+    private String nom;
+    private boolean[][] piece;
+
+    private Piece(boolean[][] piece, String nom) {
+        this.piece = piece;
+        this.nom = nom;
+    }
+
+    public boolean[][] getPiece() {
+        return this.piece;
+    }
+
+    public void afficherPiece() {
+        boolean[][] var1 = this.piece;
+        int var2 = var1.length;
+
+        for(int var3 = 0; var3 < var2; ++var3) {
+            boolean[] booleans = var1[var3];
+            StringBuilder a = new StringBuilder();
+
+            for(int j = 0; this.piece[0].length > j; ++j) {
+                if (booleans[j]) {
+                    a.append("*");
+                } else {
+                    a.append(" ");
+                }
+            }
+
+            System.out.println(a);
+        }
+
+    }
 }
---
+++ Plateau.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class Plateau {
+    private int longueur;
+    private int hauteur;
+    private boolean[][] plateau;
+
+    public Plateau(int longueur, int hauteur) {
+        this.longueur = longueur;
+        this.hauteur = hauteur;
+        this.plateau = new boolean[hauteur][longueur];
+        this.remplirPlateau();
+    }
+
+    public void placerPixel(boolean a, int x, int y) {
+        this.plateau[x][y] = a;
+    }
+
+    private void remplirPlateau() {
+        for(int i = 0; this.hauteur > i; ++i) {
+            for(int j = 0; this.longueur > j; ++j) {
+                this.placerPixel(false, i, j);
+            }
+        }
+
+    }
+
+    public void afficherPlateau() {
+        System.out.println(Arrays.deepToString(this.plateau));
+
+        for(int i = 0; this.hauteur > i; ++i) {
+            String a = "| ";
+
+            for(int j = 0; this.longueur > j; ++j) {
+                if (this.plateau[i][j]) {
+                    a = a + "* | ";
+                } else {
+                    a = a + "  | ";
+                }
+            }
+
+            System.out.println(a);
+        }
+
+    }
+}
