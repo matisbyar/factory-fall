6f1ecc4117d7e14c4dd61e231b84cb9ca1466875
ValentinG <valentin.germain01@etu.umontpellier.fr>
2022-10-14 13:14:22 +0200

    Ajout commentaires à la classe Plateau

--- Plateau.java
+++ Plateau.java
@@ -12,19 +12,21 @@ public class Plateau {
         this.remplirPlateau();
     }
 
+    /**Fonction qui sert à placer un pixel sur le plateau (vide ou non)*/
     public void placerPixel(boolean a, int y, int x) {
         this.plateau[y][x] = a;
     }
 
+    /**Fonction qui initialise le plateau pour qu'il soit vide*/
     private void remplirPlateau() {
         for(int i = 0; this.hauteur > i; ++i) {
             for(int j = 0; this.longueur > j; ++j) {
-                this.placerPixel(false, i, j);
+                this.placerPixel(false, i, j);          //place un pixel vide (état à false)
             }
         }
-
     }
 
+    /**Fonction permettant d'afficher le plateau dans le terminal (en cas de false il affiche un espace et * dans le cas d'un true)*/
     public void afficherPlateau() {
         System.out.println(Arrays.deepToString(this.plateau));
 
@@ -44,10 +46,12 @@ public class Plateau {
 
     }
 
+    /**Fonction qui permet de savoir si une ligne est supprimable, si elle est pleine,
+     la fonction appelle la fonction de suppression de ligne*/
     public void peutSupprimerLigne() {
         boolean lignePleine = false;
         int y = 19;
-        while (y < plateau.length && !lignePleine && y != 0) {
+        while (y < plateau.length && !lignePleine && y != 0) {      //les y < plateau.length && y != 0 servent a ne pas dépasser du plateau
             lignePleine = true;
             for (int x = 0; x < plateau[0].length; x++) {
                 if (!plateau[y][x]) {
@@ -61,9 +65,9 @@ public class Plateau {
             y++;
             supprimerLigne((y));
         }
-        //return lignePleine + " at " + (y);
     }
 
+    /**Fonction qui permet de supprimer une ligne d'on l'identifiant est fourni et appelle la fonction pour descendre les lignes du dessus*/
     public void supprimerLigne(int y) {
         for (int x = 0; x < plateau[0].length; x++) {
             plateau[y][x] = false;
@@ -72,17 +76,18 @@ public class Plateau {
         yDebut = y;
         int i = y;
         while (i < plateau.length) {
-            if (descendreLigne(i, yDebut)) {
+            if (descendreLigne(i, yDebut)) {    //si la ligne du dessus est vide alors la fonction s'arrête
                 break;
             }
             i--;
         }
     }
 
+    /**Fonction qui permet de descendre une ligne, elle retourne si la ligne du dessus est vide ou non*/
     public boolean descendreLigne(int y, int yDebut) {
         boolean ligneVide = true;
-        if (y != yDebut) {
-            for (int x = 0; x < plateau[0].length; x++) {
+        if (y != yDebut) {                                      //Ce if permet que la fonction ne renvoie pas true dans le premier appel
+            for (int x = 0; x < plateau[0].length; x++) {       //Cette boucle for permet de savoir si la ligne du dessus est vide
                 if (plateau[y][x]) {
                     ligneVide = false;
                     break;
@@ -92,7 +97,7 @@ public class Plateau {
             ligneVide = false;
         }
 
-        for (int x = 0; x < plateau[0].length; x++) {
+        for (int x = 0; x < plateau[0].length; x++) {       //Cette boucle descend tous les pixels de la ligne du dessus
             if (y == 0) {
                 plateau[y][x] = false;
             }
@@ -101,41 +106,46 @@ public class Plateau {
         return ligneVide;
     }
 
+    /**Fonction permettant de vérifier si la pièce donnée en paramètres, aux coordonnées données
+     entre en collision avec une autre déjà existante*/
     public boolean collisionPieces(Piece piece, int positionY, int positionX, int rotation){
-        switch (piece) {
+        switch (piece) {               //Switch case permettant de savoir quelle pièce est placé en paramètre et d'adopter le bon raisonnement
             case I :
-                switch (rotation) {
+                switch (rotation) {    //Switch case permettant de connaitre la rotation de la piece
                     case 0 :
+                        //Boucle qui vérifie si chaque case que la pièce peut potentiellement toucher est vide
                         for (int x = positionX; x < (positionX + piece.getPiece()[0].length); x++) {
-                            if (plateau[positionY][x]) {
+                            if (plateau[positionY][x]) {    //Si la case est a true alors on renvoie false (on fixe sur l'axe des Y)
                                 return false;
                             }
                         }
                         break;
                     case 90 :
+                        //Même boucle que la précédente, mais on prend y en paramètre, car la pièce est à la verticale (y décrémente parce qu'on veut placer les pixels au-dessus)
                         for (int y = positionY; y > (positionY - piece.getPiece()[0].length); y--) {
-                            if (plateau[y][positionX]) {
+                            if (plateau[y][positionX]) {    //Si la case est a true alors on renvoie false (on fixe sur l'axe des X)
                                 return false;
                             }
                         }
                         break;
                     case 180 :
                         for (int x = positionX; x < (positionX + piece.getPiece()[0].length); x++) {
-                            if (plateau[(positionY+1)][x]) {
+                            if (plateau[(positionY+1)][x]) {    //Le +1 ici sert à décaler la pièce, car d'après la documentation les pièces tournées a 0 et 180° n'ont pas la même positon
                                 return false;
                             }
                         }
                         break;
                     case 270 :
                         for (int y = positionY; y > (positionY - piece.getPiece()[0].length); y--) {
-                            if (plateau[y][positionX-1]) {
+                            if (plateau[y][positionX-1]) {  //Même problématique pour le 270 par rapport au 90° que pour le 0 et le 180°
                                 return false;
                             }
                         }
                         break;
                 }
                 break;
-            case O :
+            case O :    //Dans ce cas, on ne cherche pas à différentier les angles de rotation, car la pièce a toujours la même forme et position
+                //Les boucles imbriquées vérifient la validité du placement sur l'axe X et Y
                 for (int y = positionY; y < (positionY + piece.getPiece().length); y++) {
                     for (int x = positionX; x < (positionX + piece.getPiece()[0].length); x++) {
                         if (plateau[y][x]) {
@@ -145,6 +155,9 @@ public class Plateau {
                 }
                 break;
             case T :
+                /*Ici le cas est très similaire au deux précédents, la seule différence est que l'on cherche à savoir
+                si un pixel de la pièce est a false (dans ce cas, on ne le prend pas en compte) ou a true (dans ce cas, on le prend en compte)
+                (tous les ajouts dans les [] sont là pour ajuster graphiquement les pièces ou pour ne pas avoir d'erreurs a l'exécution*/
                 switch (rotation) {
                     case 0 :
                         for (int y = positionY; y > (positionY - piece.getPiece().length); y--) {
@@ -204,8 +217,11 @@ public class Plateau {
                         break;
                 }
                 break;
-            case S :
+            case S :        //Ici les cas S et Z sont exactement pareil pour le traitement, on peut donc fusionner les cas comme ça
             case Z :
+                /*Dans l'ensemble aucune différence fondamentale dans le traitement que dans le cas du T seul
+                des ajustements graphiques sont présents ces bouts de codes pourrait être réunis dans une fonction qui serait
+                appelée par les trois cas d'utilisation avec seulement de paramètres différents (à voir dans le futur si on a du temps)*/
                 switch (rotation) {
                     case 0 :
                         for (int y = positionY; y < (positionY + piece.getPiece().length); y++) {
@@ -270,7 +286,11 @@ public class Plateau {
                         int i = 0;
                         for (int x = positionY; x < (positionY + piece.getPiece().length); x++) {
                             for (int y = positionX; y < (positionX + piece.getPiece()[0].length); y++) {
-                                if (x-positionY == 0) {
+                                /*le if et le else servent à de l'ajustement graphique précis et unique à ce cas,
+                                car le placement de la ligne d'étoile est très différente de la colonne, ce code
+                                peut largement être amélioré, mais il fonctionne, il faut le voir comme un premier jet du a un raz le bol
+                                comme tous les autres ifs pour un cas très précis des cas suivants*/
+                                if (x-positionY == 0) { //Ce if sert à placer le pixel de la ligne {true, false, false} en bas et le premier de la ligne entièrement en true
                                     if (piece.getPiece()[x-positionY][y-positionX]) {
                                         if (plateau[x-1][y+1]) {
                                             return false;
@@ -293,7 +313,7 @@ public class Plateau {
                         for (int y = positionY+1; y > (positionY - piece.getPiece().length+1); y--) {
                             for (int x = positionX+2; x > (positionX - piece.getPiece()[0].length+2); x--) {
                                 if (piece.getPiece()[(y - positionY)][(x - positionX)]) {
-                                    if (y-positionY == 0 && x - positionX == 0) {
+                                    if (y-positionY == 0 && x - positionX == 0) {   //Ce if sert à placer le seul pixel à true de la ligne {true, false, false}
                                         if (plateau[y+1][x+1]) {
                                             return false;
                                         }
@@ -310,7 +330,7 @@ public class Plateau {
                         int z = 0;
                         for (int x = positionY; x < (positionY + piece.getPiece().length); x++) {
                             for (int y = positionX; y < (positionX + piece.getPiece()[0].length); y++) {
-                                if (x-positionY == 0) {
+                                if (x-positionY == 0) { //Ce if sert à placer le pixel de la ligne {true, false, false} en bas et le premier de la ligne entièrement en true
                                     if (piece.getPiece()[x-positionY][y-positionX]) {
                                         if (plateau[x+1][y-1]) {
                                             return false;
@@ -331,7 +351,7 @@ public class Plateau {
                         break;
                 }
                 break;
-            case L :
+            case L :        //Le raisonnement ici st similaire à celui pour le J
                 switch (rotation) {
                     case 0 :
                         for (int y = positionY; y < (positionY + piece.getPiece().length); y++) {
