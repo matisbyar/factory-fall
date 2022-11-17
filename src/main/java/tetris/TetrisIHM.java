29292cfc52ac39eb4ff974d1dc91d1ab5914d181
ValentinG <valentin.germain01@etu.umontpellier.fr>
2022-11-17 21:53:38 +0100

    ajout de la grille

--- src/main/java/tetris/TetrisIHM.java
+++ src/main/java/tetris/TetrisIHM.java
@@ -1,11 +1,31 @@
 package tetris;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-import tetris.vues.VuePlateau;
+
+
+import tetris.logique.Jeu;
+import tetris.logique.Joueur;
+import tetris.logique.Plateau;
+
+import java.awt.*;
+import java.io.File;
+import java.util.Objects;
+
 
 public class TetrisIHM extends Application {
+
+    private final Joueur j = Jeu.j;
+    private final Plateau p = Jeu.p;
+
+    GridPane gp = new GridPane();
 
     private Stage primaryStage;
 
@@ -16,9 +36,58 @@ public class TetrisIHM extends Application {
     }
 
     public void demarrerPartie() {
-        VuePlateau plateau = new VuePlateau();
-        Scene scene = new Scene(plateau);
+        //VuePlateau plateau = new VuePlateau();
+        Scene scene = new Scene(afficherplateau());
         primaryStage.setScene(scene);
         primaryStage.show();
+        Jeu.nouvellePieceActuelle();
+        afficherplateau();
+        primaryStage.show();
+    }
+
+    public GridPane afficherplateau() {
+        for (int i = 0; i < p.getPlateau().length; i++) {
+            for (int y = 0; y < p.getPlateau()[0].length; y++) {
+                TextField tf = new TextField();
+                tf.setPrefHeight(45);
+                tf.setPrefWidth(45);
+                tf.setAlignment(Pos.CENTER);
+                tf.setEditable(false);
+                String s = p.getPlateau()[i][y].getNom();
+                switch (s) {
+                    case "S" :
+                        tf.setStyle("-fx-background-color: limegreen;");
+                        break;
+                    case "I" :
+                        tf.setStyle("-fx-background-color: #00bbff;");
+                        break;
+                    case "J" :
+                        tf.setStyle("-fx-background-color: blue;");
+                        break;
+                    case "L" :
+                        tf.setStyle("-fx-background-color: orange;");
+                        break;
+                    case "O" :
+                        tf.setStyle("-fx-background-color: yellow;");
+                        break;
+                    case "T" :
+                        tf.setStyle("-fx-background-color: purple;");
+                        break;
+                    case "Z" :
+                        tf.setStyle("-fx-background-color: red;");
+                        break;
+                }
+                tf.setText(s);
+                //tf.setStyle("-fx-background-color: limegreen;");
+                GridPane.setRowIndex(tf,i);
+                GridPane.setColumnIndex(tf,y);
+                gp.getChildren().add(tf);
+            }
+        }
+        return gp;
+    }
+
+    private String test(String path) {
+        return Objects.requireNonNull(getClass().getResource(path)).toString();
     }
 }
