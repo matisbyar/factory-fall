368a8c8e95fdc17af5a754dace3785a06d425248
PIERREVELCIN Alban <alban.pierrevelcin@etu.umontpellier.fr>
2022-11-18 12:29:11 +0100

    modifs sur le pom et le module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,8 @@
 module tetris {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires com.almasb.fxgl.all;
+    requires javafx.base;
     requires java.desktop;
-    requires javafx.graphics;
 
     opens tetris to javafx.fxml;
     exports tetris;
