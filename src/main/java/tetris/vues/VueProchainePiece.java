8e0f1499d5d57db8f63ed17c5829e7b0bdfd93e9
ValentinG <valentin.germain01@etu.umontpellier.fr>
2022-12-11 17:19:27 +0100

    Ajout des nouvelles pièces + modification de vue plateau et prochaines pièces pour prendre les images

--- src/main/java/tetris/vues/VueProchainePiece.java
+++ src/main/java/tetris/vues/VueProchainePiece.java
@@ -10,13 +10,13 @@ import tetris.logique.Plateau;
 public class VueProchainePiece extends GridPane {
 
     private final Image vide = new Image("file:src/main/resources/img/vide.png");
-    private final Image imgS = new Image("file:src/main/resources/img/S.jpg");
-    private final Image imgI = new Image("file:src/main/resources/img/I.jpg");
-    private final Image imgJ = new Image("file:src/main/resources/img/J.jpg");
-    private final Image imgL = new Image("file:src/main/resources/img/L.jpg");
-    private final Image imgO = new Image("file:src/main/resources/img/O.jpg");
-    private final Image imgT = new Image("file:src/main/resources/img/T.jpg");
-    private final Image imgZ = new Image("file:src/main/resources/img/Z.jpg");
+    private final Image imgS = new Image("file:src/main/resources/img/conteneur/S.jpg");
+    private final Image imgI = new Image("file:src/main/resources/img/conteneur/I.jpg");
+    private final Image imgJ = new Image("file:src/main/resources/img/conteneur/J.jpg");
+    private final Image imgL = new Image("file:src/main/resources/img/conteneur/L.jpg");
+    private final Image imgO = new Image("file:src/main/resources/img/conteneur/O.jpg");
+    private final Image imgT = new Image("file:src/main/resources/img/conteneur/T.jpg");
+    private final Image imgZ = new Image("file:src/main/resources/img/conteneur/Z.jpg");
 
     Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
 
