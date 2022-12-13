c2dc904648c9c06b9936010b596eab18095be432
matisbyar <matis.byar.06@icloud.com>
2022-12-13 19:00:34 +0100

    Une colonne sur 2 est plus claire + ratio sur la résolution

--- src/main/java/tetris/vues/VuePlateau.java
+++ src/main/java/tetris/vues/VuePlateau.java
@@ -5,13 +5,12 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Screen;
-import org.w3c.dom.ls.LSOutput;
-import tetris.TetrisIHM;
 import tetris.logique.Plateau;
 
 public class VuePlateau extends GridPane {
 
     private final Image vide = new Image("file:src/main/resources/img/vide.png");
+    private final Image vide_clair = new Image("file:src/main/resources/img/vide_clair.png");
     private final Image imgS = new Image("file:src/main/resources/img/conteneur/S.png");
     private final Image imgI = new Image("file:src/main/resources/img/conteneur/I.png");
     private final Image imgJ = new Image("file:src/main/resources/img/conteneur/J.png");
@@ -34,18 +33,11 @@ public class VuePlateau extends GridPane {
         // Ajoute les pièces dans la grille
         for (int colonne = 0; colonne < p.getPlateau().length; colonne++) {
             for (int ligne = 0; ligne < p.getPlateau()[0].length; ligne++) {
-                ImageView imagePiece = new ImageView(vide);
+                ImageView imagePiece = ligne % 2 == 0 ? new ImageView(vide) : new ImageView(vide_clair);
 
-                if (primaryScreenBounds.getWidth() == 1920){
-                    imagePiece.setFitHeight((primaryScreenBounds.getHeight()*0.03));
-                    imagePiece.setFitWidth((primaryScreenBounds.getHeight()*0.03));
-                }else if(primaryScreenBounds.getWidth() == 1280){
-                    imagePiece.setFitHeight((primaryScreenBounds.getHeight()*0.046));
-                    imagePiece.setFitWidth((primaryScreenBounds.getHeight()*0.046));
-                }else if(primaryScreenBounds.getWidth() == 1366){
-                    imagePiece.setFitHeight((primaryScreenBounds.getHeight()*0.043));
-                    imagePiece.setFitWidth((primaryScreenBounds.getHeight()*0.043));
-                }
+                imagePiece.setFitHeight((primaryScreenBounds.getHeight() * 0.03));
+                imagePiece.setFitWidth((primaryScreenBounds.getHeight() * 0.03));
+
                 String nomPiece = p.getPlateau()[colonne][ligne].getNom();
                 switch (nomPiece) {
                     case "S" -> imagePiece.setImage(imgS);
@@ -57,6 +49,7 @@ public class VuePlateau extends GridPane {
                     case "Z" -> imagePiece.setImage(imgZ);
                 }
                 this.add(imagePiece, ligne, colonne);
+
             }
         }
     }
