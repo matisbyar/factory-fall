960fd21d0bbb2ef4da99056502a711312b217849
matisbyar <matis.byar.06@icloud.com>
2023-01-06 21:27:32 +0100

    Ajout du style pour la barre de navigation.
    Disclaimer : je n'ai pas réussi à centrer le titre dû aux propriétés du HBox.

--- src/main/java/tetris/vues/helpers/BarreNavigation.java
+++ src/main/java/tetris/vues/helpers/BarreNavigation.java
@@ -3,7 +3,11 @@ package tetris.vues.helpers;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import tetris.TetrisIHM;
+import tetris.logique.Preferences;
 import tetris.vues.Menu;
+
+import java.util.Objects;
 
 /**
  * La BarreNavigation est un helper qui permet la réutilisation de la même barre de navigation sur les pages du menu du jeu. Un "helper" est un fragment de vue qui factorise le code.
@@ -12,6 +16,11 @@ import tetris.vues.Menu;
  * Mais également un titre qui permet d'identifier la page sur laquelle on se trouve.
  */
 public class BarreNavigation extends HBox {
+
+    /**
+     * Le titre de la page (vueActuelle)
+     */
+    private final Label titre;
 
     /**
      * Le bouton retour permet de revenir à la page précédente.
@@ -26,19 +35,34 @@ public class BarreNavigation extends HBox {
     /**
      * Constructeur de la barre de navigation.
      *
-     * @param titreVue         Le titre de la page sur laquelle on se trouve.
+     * @param titreVue      Le titre de la page sur laquelle on se trouve.
      * @param vuePrecedente La vue précédente, qui sera affichée lorsque l'on cliquera sur le bouton retour.
      * @param vueActuelle   La vue actuelle (celle sur laquelle le bouton retour se trouve).
      */
     public BarreNavigation(String titreVue, Menu vuePrecedente, Menu vueActuelle) {
-        Label titre = new Label(titreVue);
+        this.titre = new Label(titreVue);
         this.retour = new Button("Retour");
         this.vuePrecedente = vuePrecedente;
         this.vueActuelle = vueActuelle;
 
+        styliser();
         retour();
 
         this.getChildren().addAll(retour, titre);
+    }
+
+    protected void styliser() {
+        this.getStylesheets().add(Objects.requireNonNull(TetrisIHM.class.getResource("css/menu.css")).toString());
+
+        this.getStyleClass().add("barre-navigation");
+        retour.getStyleClass().add("bouton-clair");
+        titre.getStyleClass().add("titre");
+
+        titre.setFont(Preferences.getInstance().getPolice(70));
+        retour.setFont(Preferences.getInstance().getPolice(50));
+
+        HBox.setMargin(retour, new javafx.geometry.Insets(0, 50, 0, 0));
+
     }
 
     /**
