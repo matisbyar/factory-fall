e58eb015555f657234bb0ad592751cfa4fe8f23e
matisbyar <matis.byar.06@icloud.com>
2023-01-09 14:55:34 +0100

    Ajout d'une classe singleton Ressources, distincte de Preferences qui gère toutes les ressources du jeu (la police migrant notamment vers cette classe)

---
+++ src/main/java/tetris/singletons/Ressources.java
@@ -0,0 +1,54 @@
+package tetris.singletons;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Font;
+import tetris.TetrisIHM;
+
+import java.util.Objects;
+
+/**
+ * À la différence de la classe Preferences, le singleton Ressources charge toutes les ressources du jeu
+ */
+public class Ressources {
+
+    private static Ressources INSTANCE = null;
+
+    // Pièces affichées dans MenuPersonnalisation
+    private final Image pieceDefault = new Image(Objects.requireNonNull(TetrisIHM.class.getResourceAsStream("img/default/L.jpg")));
+    private final Image pieceConteneur = new Image(Objects.requireNonNull(TetrisIHM.class.getResourceAsStream("img/conteneur/L.jpg")));
+    private final Image pieceBrique = new Image(Objects.requireNonNull(TetrisIHM.class.getResourceAsStream("img/brique/L.jpg")));
+
+    // Images des boutons
+    private final ImageView gauche = new ImageView(new Image(Objects.requireNonNull(TetrisIHM.class.getResourceAsStream("img/flechePersonnalisation/fleche-gauche.png"))));
+    private final ImageView droite = new ImageView(new Image(Objects.requireNonNull(TetrisIHM.class.getResourceAsStream("img/flechePersonnalisation/fleche-droite.png"))));
+
+    public static Ressources getInstance() {
+        if (INSTANCE == null) INSTANCE = new Ressources();
+        return INSTANCE;
+    }
+
+    public Font getPolice(int taille) {
+        return Font.loadFont(Objects.requireNonNull(TetrisIHM.class.getResourceAsStream("fonts/arcade.ttf")), taille);
+    }
+
+    public Image getPieceDefault() {
+        return pieceDefault;
+    }
+
+    public Image getPieceConteneur() {
+        return pieceConteneur;
+    }
+
+    public Image getPieceBrique() {
+        return pieceBrique;
+    }
+
+    public ImageView getFlecheGauche() {
+        return gauche;
+    }
+
+    public ImageView getFlecheDroite() {
+        return droite;
+    }
+}
